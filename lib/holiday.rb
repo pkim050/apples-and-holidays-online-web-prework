bccc7480a040b1b653333b0223effba20e0952ec
Patrick Kim <pkim050@gmail.com>
2019-02-11 08:42:28 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,31 +18,33 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+  holiday_hash[:winter][:christmas] << supply
+  holiday_hash[:winter][:new_years] << supply
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+  holiday_hash[:spring][:memorial_day] << supply
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-
+  holiday_hash[season][holiday_name] = supply_array
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  holiday_hash[:winter].values_at(:christmas, :new_years).flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -53,13 +55,42 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |k, v|
+    temp = ""
+    temp = k.to_s
+    temp.capitalize!
+    puts "#{temp}:"
+    v.each do |k2, v2|
+      temp2 = ""
+      temp2 = k2.to_s
+      if temp2.include? "_"
+        temp2 = temp2.split('_')
+        temp2.map! {|i| i.capitalize!}
+        temp2 = temp2.join(" ")
+      else
+        temp2.capitalize!
+      end
+      temp3 = ""
+      temp3 = v2.join(", ")
+      puts "  #{temp2}: #{temp3}"
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  temp = []
+  holiday_hash.each do |k, v|
+    v.each do |k2, v2|
+      v2.each do |i|
+        if i == "BBQ"
+          temp << k2
+        end
+      end
+    end
+  end
+  temp
 end
 
 
